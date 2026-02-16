be8674cf4e6c7c714a1de1b6684dbcd1d03352e3
William Phan <phnam230197@gmail.com>
2026-02-17 05:42:40 +0700

    chore: update settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,8 +2,23 @@ import java.lang.System.getenv
 
 rootProject.name = "algorithm"
 
+val moduleDeps = mapOf(
+    "core" to emptyList(),
+    "reactive" to listOf("core"),
+    "spring-boot-starter" to listOf("core"),
+    "spring-boot-starter-reactive" to listOf("reactive"),
+)
+
+val included = mutableSetOf<String>()
+
+fun includeRecursive(module: String) =
+    module
+        .takeIf { included.add(it) }
+        ?.also { include(it) }
+        ?.let { moduleDeps[it].orEmpty().forEach(::includeRecursive) }
+
 getenv("MODULE")?.takeIf { it.isNotBlank() }?.let {
-    include(it)
+    includeRecursive(it)
 } ?: include(
     "core",
     "reactive",
