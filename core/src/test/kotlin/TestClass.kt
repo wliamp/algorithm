868669ee446190e0a0b2990968ae3e82e1192ece
William Phan <phnam230197@gmail.com>
2026-02-17 09:40:10 +0700

    chore: remove unnecessary Kotlin package folder

--- core/src/test/kotlin/TestClass.kt
+++ core/src/test/kotlin/TestClass.kt
@@ -1,7 +1,7 @@
 package io.github.wliamp.kit.agr.core
 
 import io.github.wliamp.kit.agr.core.Criteria.alwaysTrue
-import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 class TestClass {
--- spring-boot-starter-reactive/src/main/kotlin/ReactiveAlgorithmAutoConfig.kt
+++ spring-boot-starter-reactive/src/main/kotlin/ReactiveAlgorithmAutoConfig.kt
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean
 
 @AutoConfiguration
-class AlgorithmReactiveAutoConfig {
+class ReactiveAlgorithmAutoConfig {
     @Bean
     @ConditionalOnMissingBean
     fun imperativeQueue(): Queue<Any> = Queue()
